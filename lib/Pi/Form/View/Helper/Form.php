9ab9f61aaf522b0a3d50b2b85be6c61a79c0c16a
Mickaël STAMM <contact@sta2m.com>
2020-07-01 17:23:14 +0100

    https://github.com/pi-engine/guide/issues/1914

--- lib/Pi/Form/View/Helper/Form.php
+++ lib/Pi/Form/View/Helper/Form.php
@@ -604,8 +604,8 @@ EOT;
 <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-hidden="true" title="%s">&times;</button>
             <h4 class="modal-title">%s</h4>
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true" title="%s">&times;</button>
         </div>
         <div class="modal-body">
 EOT;
